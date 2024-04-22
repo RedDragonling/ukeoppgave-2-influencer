a129906fe81425904b244f679e6549bfc8d7c5e0
Eddie Nilsen <eddienilsen0506@gmail.com>
2024-04-22 13:20:48 +0200

    ts fail

---
+++ src/navbar.ts
@@ -0,0 +1,8 @@
+console.log('why');
+// const logo = document.querySelectorAll('.logo-title');
+
+// logo.forEach( element =>{
+//   element.addEventListener('click', function() {
+//     window.location.href = 'index.html';
+//   });
+// });
